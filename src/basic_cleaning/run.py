cbed4ea57e030987912653b449e4ed1d8d02a0eb
Orlando <trejo10@gmail.com>
2025-02-25 22:34:17 -0800

    Update basic_cleaning to process sample2 data

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,9 @@ def go(args):
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save the cleaned dataset to csv file
     df.to_csv("clean_sample.csv", index=False)
 
